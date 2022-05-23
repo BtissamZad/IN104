729e067a6553f5ffcec2b5b4f08f8899f96ff413
Zadouani Ibtissam <ibtissam.zadouani@ensta-paris.fr>
2022-05-23 11:10:51 +0200

    test commit version 1

---
+++ ParcoursArbre AST.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct racine {
+    int data ; char caractere; char* expression ; double reel; bool verite;
+};
+
+struct bnode {
+    struct racine root;
+    struct bnode *left;
+    struct bnode *right;
+}
+
+void  infix (struct  bnode *n)
+{
+    if (n != NULL) {
+        infix (n->left) ;
+        printf (*(n->root)) ;
+        infix (n->right) ;
+    }
+}
